52f84851d3ae1ca3752e5389b1f6abab468a17e2
Rishaub Kumar <rishaubkumar534@gmail.com>
2026-06-24 22:23:49 +0530

    remove write to server.log

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,14 +16,11 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Database connected"))
     .catch(err => console.error(err));
 
-const fs = require('fs');
-
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-  const log = `${new Date().toISOString()} ${req.method} ${req.url}\n`;
-  fs.appendFileSync('server.log', log);
-  console.log(log.trim());
+  const log = `${new Date().toISOString()} ${req.method} ${req.url}`;
+  console.log(log);
   next();
 });
 
